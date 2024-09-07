996dd22d09b6665ba5d6785dcc4518fa3c33628b
Angus Gratton <gus@projectgus.com>
2024-09-07 13:31:28 +1000

    pac: Split TXBCIE and TXBTIE regs between H7 and G0/G4/L5.
    Different bit widths.

---
+++ src/pac/fdcan/txbtie_g0_g4_l5.rs
@@ -0,0 +1,106 @@
+///Register `TXBTIE` reader
+pub struct R(crate::R<TXBTIE_SPEC>);
+impl core::ops::Deref for R {
+    type Target = crate::R<TXBTIE_SPEC>;
+    #[inline(always)]
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl From<crate::R<TXBTIE_SPEC>> for R {
+    #[inline(always)]
+    fn from(reader: crate::R<TXBTIE_SPEC>) -> Self {
+        R(reader)
+    }
+}
+///Register `TXBTIE` writer
+pub struct W(crate::W<TXBTIE_SPEC>);
+impl core::ops::Deref for W {
+    type Target = crate::W<TXBTIE_SPEC>;
+    #[inline(always)]
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl core::ops::DerefMut for W {
+    #[inline(always)]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+impl From<crate::W<TXBTIE_SPEC>> for W {
+    #[inline(always)]
+    fn from(writer: crate::W<TXBTIE_SPEC>) -> Self {
+        W(writer)
+    }
+}
+///Field `TIE` reader - Transmission Interrupt Enable
+pub struct TIE_R(crate::FieldReader<u32, u32>);
+impl TIE_R {
+    pub(crate) fn new(bits: u32) -> Self {
+        TIE_R(crate::FieldReader::new(bits))
+    }
+}
+impl core::ops::Deref for TIE_R {
+    type Target = crate::FieldReader<u32, u32>;
+    #[inline(always)]
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+///Field `TIE` writer - Transmission Interrupt Enable
+pub struct TIE_W<'a> {
+    w: &'a mut W,
+}
+impl<'a> TIE_W<'a> {
+    ///Writes raw bits to the field
+    #[inline(always)]
+    pub unsafe fn bits(self, value: u32) -> &'a mut W {
+        self.w.bits = (self.w.bits & !0x7) | (value as u32 & 0x7);
+        self.w
+    }
+}
+impl R {
+    ///Bits 0:2 - Transmission Interrupt Enable
+    #[inline(always)]
+    pub fn tie(&self) -> TIE_R {
+        TIE_R::new((self.bits & 0x7) as u32)
+    }
+}
+impl W {
+    ///Bits 0:2 - Transmission Interrupt Enable
+    #[inline(always)]
+    pub fn tie(&mut self) -> TIE_W {
+        TIE_W { w: self }
+    }
+    ///Writes raw bits to the register.
+    #[inline(always)]
+    pub unsafe fn bits(&mut self, bits: u32) -> &mut Self {
+        self.0.bits(bits);
+        self
+    }
+}
+///FDCAN Tx Buffer Transmission Interrupt Enable Register
+///
+///This register you can [`read`](crate::generic::Reg::read), [`write_with_zero`](crate::generic::Reg::write_with_zero), [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`modify`](crate::generic::Reg::modify). See [API](https://docs.rs/svd2rust/#read--modify--write-api).
+///
+///For information about available fields see [txbtie](index.html) module
+pub struct TXBTIE_SPEC;
+impl crate::RegisterSpec for TXBTIE_SPEC {
+    type Ux = u32;
+}
+///`read()` method returns [txbtie::R](R) reader structure
+impl crate::Readable for TXBTIE_SPEC {
+    type Reader = R;
+}
+///`write(|w| ..)` method takes [txbtie::W](W) writer structure
+impl crate::Writable for TXBTIE_SPEC {
+    type Writer = W;
+}
+///`reset()` method sets TXBTIE to value 0
+impl crate::Resettable for TXBTIE_SPEC {
+    #[inline(always)]
+    fn reset_value() -> Self::Ux {
+        0
+    }
+}
